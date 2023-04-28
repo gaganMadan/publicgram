0227e1420f0c0e034dd549e83dcfb1c8d237493e
Gagan <gagan7535@gmail.com>
2023-04-28 20:01:37 +0530

    text update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
+      We’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Anshu</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Gagan</a>. What's next?
     </h3>
