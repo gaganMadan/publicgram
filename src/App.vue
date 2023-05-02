f687865d5c910f62651ffdef4267831eb3a7da87
Abhishek Anshu <abhianshu2020@gmail.com>
2023-05-02 15:30:28 +0530

    fix(build)

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,40 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+// import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <nav>
+      <div class="container is-fluid" style="height: 100vh">
+          <div class="columns m-auto" style=" height: 100%;">
+            <div class="column is-narrow sidebar pl-0" style="width: 400px;">
+              <p class="brand">Publicgram</p>
+            </div>
+            <div class="column">second column</div>
+        </div>
+      </div>
+ 
+      <!-- <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-      </nav>
+      </nav> -->
     </div>
   </header>
 
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
 
 <style>
-@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+@import 'https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css';
+@import url('https://fonts.googleapis.com/css2?family=Sacramento&display=swap');
 
+.brand {
+  font-family: 'Sacramento', cursive;
+  font-size: 3rem;
+  color: black;
+}
+
+.sidebar {
+  border-right: 1px solid #CACACA;
+}
 </style>
