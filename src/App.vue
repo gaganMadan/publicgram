9cdad22ee70881064acbdceb3fcee715d05bdd85
Abhishek Anshu <abhianshu2020@gmail.com>
2023-05-09 17:19:43 +0530

    add modal to create post

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,29 @@
 </script>
 
 <template>
+  <div class="modal" style="display: block;">
+    <div class="modal-background"></div>
+    <div class="modal-content" style="height: 100%;">
+      <div id="modal" 
+        class="is-flex is-flex-direction-column is-justify-content-center is-align-items-center"
+        style="min-height: 100%">
+      </div>
+    </div>
+    <button class="modal-close is-large" aria-label="close"></button>
+  </div>
+
   <header>
     <div class="wrapper">
-      <div class="container is-fluid" style="height: 100vh">
+      <div class="container is-fluid" style="min-height: 100vh">
         <RouterView />
       </div>
- 
+
       <!-- <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav> -->
     </div>
   </header>
-
 </template>
 
 <style>
--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -4,10 +4,16 @@
   <p class="brand has-text-weight-bold">Publicgram</p>
   <ul class="menu-list mt-6">
     <li><a>Search</a></li>
-    <li><a>Explore</a></li>
+    <li><a>Create</a></li>
     <li><a>Message</a></li>
     <li><a>Notification</a></li>
   </ul>
+
+  <Teleport to="#modal">
+    <div class="create-post_dialog p-4">
+      <h1 class="is-size-4">What's the vibe?</h1>
+    </div>
+  </Teleport>
 </template> 
 
 <style>
@@ -15,5 +21,11 @@
   font-family: 'Sacramento', cursive;
   font-size: 2rem;
   color: black;
+}
+
+.create-post_dialog {
+  min-width: 500px;
+  min-height: 500px;
+  background-color: white;
 }
 </style>
